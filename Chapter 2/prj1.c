22bd7199591c52b7d55daa593f18b4dcb34fd74b
berkpereira <85910574+berkpereira@users.noreply.github.com>
2021-08-19 20:11:55 +0100

    Update prj1.c

--- Chapter 2/prj1.c
+++ Chapter 2/prj1.c
@@ -1,1 +1,17 @@
+//
+//  prj1.c
+//  
+//
+//  Created by Gabriel Pereira on 11/08/2021.
+//
 
+// Prints a checkmark to the command line using * character
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("       *\n      *\n     *\n*   *\n * *\n  *\n");
+	
+    return 0;
+}
